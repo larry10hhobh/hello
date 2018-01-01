3e2ac4e7c528da7a48532f554ca4be5e4a7b91b9
Larry <larry10hhobh@gmail.com>
2018-01-01 23:01:58 +0800

    Initial commit

---
+++ src/com/shu/example/Hello.java
@@ -0,0 +1,10 @@
+package com.shu.example;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World！！！");
+/*        for (int i = 0; i < 100000; i++) {
+            System.out.println("Hello World!");
+        }*/
+    }
+}
